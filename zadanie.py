b21101aa31eb040edc3e04f4a341775783482482
Guldier <damianjadacki@gmail.com>
2019-03-17 09:59:22 +0100

    Dodano zadanie challenge

---
+++ zadanie.py
@@ -0,0 +1,16 @@
+
+
+text = list("""g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr 
+ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.""")
+
+
+alfabet = list("abcdefghijklmnopqrstuvwxyzab")
+
+# for i in range(0, len(text) - 1):
+#     for j in range(0, len(alfabet) - 1):
+#         if text[i] == alfabet[j]:
+#             text[i] = alfabet[j + 2]
+#             break
+
+
+print("".join(text))
